a29adcc1dd4b92b54f24fb354be7b0b21153a5c5
Ibrahim Ay <ibrahim.ay@student.hs-rm.de>
2021-05-01 15:49:24 +0200

    füge sichtung/meine GetMapping Methode hinzu

--- src/main/java/de/hsrm/mi/web/projekt/sichtung/SichtungController.java
+++ src/main/java/de/hsrm/mi/web/projekt/sichtung/SichtungController.java
@@ -1,5 +1,20 @@
 package de.hsrm.mi.web.projekt.sichtung;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
 public class SichtungController {
-    
+    Logger logger = LoggerFactory.getLogger(SichtungController.class);
+
+    @GetMapping("/sichtung/meine")
+    public String sichtungMeineGet() {
+        return "meinesichtung";
+    }
+
 }
