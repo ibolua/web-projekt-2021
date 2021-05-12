aaeb860d4ba6a9637ea752b3e57feb69a64882fc
Ibrahim Ay <ibrahim.ay@student.hs-rm.de>
2021-05-12 12:31:16 +0200

    SessionAttribute korrigiert und Aufräumaktionen

--- src/main/java/de/hsrm/mi/web/projekt/sichtung/SichtungController.java
+++ src/main/java/de/hsrm/mi/web/projekt/sichtung/SichtungController.java
@@ -9,28 +9,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping("/sichtung")
 // Attribut 'meinesichtungen' im Model soll im Session-Scope landen
-@SessionAttributes(names = { "meinesichtungen", "loggedinusername" })
+@SessionAttributes(names = { "meinesichtungen" })
 public class SichtungController {
     Logger logger = LoggerFactory.getLogger(SichtungController.class);
     private static final List<String> HEADERS = Arrays.asList("Datum", "Name", "Ort", "Beschreibung");
-
 
     // Model-Attribut 'meinesichtungen' initialisieren (Session-Scope!)
     @ModelAttribute("meinesichtungen")
     public void initListe(Model m, @ModelAttribute("loggedinusername") String loggedinusername) {
         ArrayList<Sichtung> dieSicht = new ArrayList<>();
-        
+
         LocalDate localDate = LocalDate.now();
 
         Sichtung a = new Sichtung();
@@ -43,7 +42,6 @@ public class SichtungController {
         a.setOrt("Vollradisroda");
         a.setDatum(localDate);
         a.setBeschreibung("Coole 17 am Ortseingang");
-
 
         b.setName("B_Herta");
         b.setOrt("Wuppertal");
@@ -76,29 +74,13 @@ public class SichtungController {
         m.addAttribute("headers", HEADERS);
     }
 
-    // @GetMapping("/meine/liste")
-    // public String sichtungenMeineListeGet(Model m, @ModelAttribute("loggedinusername") String loggedinusername){
-    //     logger.error("Sichtung GetMapping LISTE Username: {}", loggedinusername);
-    //     m.addAttribute("willkommenUser", "Willkommen " + loggedinusername);
-    //     return "liste";
-    // }
-    // @GetMapping("/meine/liste")
-    // public String sichtungenMeineListeGet(){
-    //     return "liste";
-    // }
-
-    // @GetMapping("/meine")
-    // public String sichtungMeineGet() {
-    //     return "redirect:/sichtung/meine/liste";
-    // }
-
     @GetMapping("/meine")
-    public String sichtungMeineGet(Model m, @ModelAttribute("loggedinusername") String loggedinusername){
+    public String sichtungMeineGet(Model m, @ModelAttribute("loggedinusername") String loggedinusername) {
         m.addAttribute("willkommenUser", "Willkommen " + loggedinusername);
         m.addAttribute("headers", HEADERS);
         return "sichtung/meine/liste";
     }
-    
+
     @GetMapping("/meine/neu")
     public String sichtungMeineNeuGet(Model m) {
         m.addAttribute("meinesichtungform", new Sichtung());
@@ -106,40 +88,34 @@ public class SichtungController {
     }
 
     @PostMapping("/meine/neu")
-    // @ModelAttribute("meinesichtungen")
-    public String sichtungMeineNeuPost(
-        @ModelAttribute("meinesichtungform") Sichtung sichtung, Model m,
-        @ModelAttribute("meinesichtungen") List<Sichtung> dieSichtungen,
-        ModelMap model) {
+    public String sichtungMeineNeuPost(@ModelAttribute("meinesichtungform") Sichtung sichtung, Model m,
+            @ModelAttribute("meinesichtungen") List<Sichtung> dieSichtungen) {
         dieSichtungen.add(sichtung);
-
-        logger.warn("/meine/neu loggedinusername = {}", model.get("loggedinusername"));
-        // m.addAttribute("meinesichtungen", dieSichtungen);
         return "redirect:/sichtung/meine";
     }
 
-
     @GetMapping("/meine/{index}")
-    public String editGet(
-        @ModelAttribute("meinesichtungform") Sichtung sichtung, Model m,
-        @ModelAttribute("meinesichtungen") List<Sichtung> dieSichtungen,
-        @PathVariable int index) {
+    public String editGet(@ModelAttribute("meinesichtungform") Sichtung sichtung, Model m,
+            @ModelAttribute("meinesichtungen") List<Sichtung> dieSichtungen, @PathVariable int index) {
 
         Sichtung aktSichtung = dieSichtungen.get(0);
         m.addAttribute("meinesichtungform", aktSichtung);
         logger.warn("/mein/index aktuelleSichtung: {}", aktSichtung.getName());
-        // dieSichtungen.remove(0);
         dieSichtungen.remove(index);
         return "redirect:/sichtung/meine/neu";
     }
-    
-    @GetMapping("/meine/{index}/del")
-    public String deleteGet(
-        @ModelAttribute("meinesichtungen") List<Sichtung> dieSichtungen,
-        @PathVariable int index) {
 
-            // dieSichtungen.remove(0);
-            dieSichtungen.remove(index);
-            return "redirect:/sichtung/meine";
+    @GetMapping("/meine/{index}/del")
+    public String deleteGet(@ModelAttribute("meinesichtungen") List<Sichtung> dieSichtungen, @PathVariable int index) {
+        dieSichtungen.remove(index);
+        return "redirect:/sichtung/meine";
+    }
+
+    @GetMapping("/logout")
+    public String logout(SessionStatus sessionstatus) {
+        // Session aktiv beenden
+        // Eigentlich unnötig, aber VS Code meckert rum
+        sessionstatus.setComplete();
+        return "login";
     }
 }
