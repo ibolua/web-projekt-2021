43119a9a1eafd8abb9876088b8d03ab31ffdf54b
Ibrahim Ay <ibrahim.ay@student.hs-rm.de>
2021-05-22 00:18:24 +0200

    FotoService ergänzt

--- src/main/java/de/hsrm/mi/web/projekt/foto/FotoServiceImpl.java
+++ src/main/java/de/hsrm/mi/web/projekt/foto/FotoServiceImpl.java
@@ -1,7 +1,10 @@
 package de.hsrm.mi.web.projekt.foto;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,6 +50,57 @@ public class FotoServiceImpl implements FotoService {
     @Override
     public void loescheFoto(Long id) {
         fotoRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public void fotoKommentieren(long id, String autor, String kommentar) throws NoSuchElementException {
+        // TODO Irgendwie hässlich. Bitte morgen aufräumen. Ist jetzt zu spät geworden.
+        Optional<Foto> fotoopt = fotoRepository.findById(id);
+        var foto = fotoopt.orElseThrow();
+        var k = new Kommentar();
+        k.setAutor(autor);
+        k.setText(kommentar);
+        List<Kommentar> kommentare = foto.getKommentare();
+        kommentare.add(k);
+        foto.setKommentare(kommentare);
+    }
+
+    @Override
+    @Transactional
+    public List<Kommentar> alleKommentareFuerFoto(long fotoid) throws NoSuchElementException {
+        // TODO Bitte morgen aufräumen. Ist jetzt zu spät geworden.
+
+        Optional<Foto> fotoopt = fotoRepository.findById(fotoid);
+        var foto = fotoopt.orElseThrow();
+
+        List<Kommentar> kommentare = foto.getKommentare();
+
+        return kommentare;
+    }
+
+    @Override
+    @Transactional
+    public void fotoKommentarLoeschen(long fotoid, long kid) throws NoSuchElementException {
+        // TODO Hässlicher Code, vorallem unten mit der for-Schleife und dem throw new. Bitte aufräumen
+
+        Optional<Foto> fotoopt = fotoRepository.findById(fotoid);
+        Foto foto = fotoopt.orElseThrow();
+        // Kommentar kommentar = foto.getKommentare().get(kid);
+
+        List<Kommentar> kommentare = foto.getKommentare();
+        // kommentare.remove(kommentar);
+
+        
+        for(Kommentar kommentar : kommentare) {
+            // if(kommentar.getId() == kid) {
+            if(kommentar.getId() == kid ) {
+                kommentare.remove(kommentar);
+                return;
+            }
+        }
+        throw new NoSuchElementException();
+
     }
     
 }
