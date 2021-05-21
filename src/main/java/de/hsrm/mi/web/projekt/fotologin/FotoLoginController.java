44baf3f577fd7df4eb6013ca8266c648fe7b68fe
Ibrahim Ay <ibrahim.ay@student.hs-rm.de>
2021-05-21 18:42:02 +0200

    Create and Edit FotoLogin Controller/Template/CSS

---
+++ src/main/java/de/hsrm/mi/web/projekt/fotologin/FotoLoginController.java
@@ -0,0 +1,72 @@
+package de.hsrm.mi.web.projekt.fotologin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+@SessionAttributes(names = { "loggedinusername" })
+public class FotoLoginController {
+    private static final String LOGING_STRING = "foto/login"; // Compliant
+    private static final String REDIRECT_SICHTUNG_MEINE_STRING = "redirect:/foto"; // Compliant
+    private static final String LOGGEDINUSERNAME_STRING = "loggedinusername"; // Compliant
+    Logger logger = LoggerFactory.getLogger(FotoLoginController.class);
+
+    // Mit @ModelAttribute annotierte Methoden werden vor Requesthandler Methoden
+    // aufgerufen
+    // Model-Attribut 'loggedinusername' initialisieren (Session-Scope!)
+    @ModelAttribute(LOGGEDINUSERNAME_STRING)
+    public void initLoggedInUser(Model m) {
+        m.addAttribute(LOGGEDINUSERNAME_STRING, "");
+    }
+
+    // GET auf http://localhost:8080/login
+    @GetMapping("/fotologin")
+    public String loginGet(@ModelAttribute(LOGGEDINUSERNAME_STRING) String loggedinusername) {
+
+        logger.info("GET LOGIN {}", loggedinusername);
+        if (!loggedinusername.isEmpty()) {
+            return REDIRECT_SICHTUNG_MEINE_STRING;
+        }
+        return LOGING_STRING;
+    }
+
+    @PostMapping("/fotologin")
+    public String loginPost(Model m, @RequestParam String username, @RequestParam String password) {
+
+        String pwMitLaenge = username + username.length();
+        // String hinweis = "Hinweis: Das korrekte Passwort für " + username + " ist " + pwMitLaenge;
+
+        if (username.isEmpty()) {
+            return LOGING_STRING;
+        }
+
+        if (password.equals(pwMitLaenge)) {
+            m.addAttribute("willkommenUser", "Willkommen " + username);
+            m.addAttribute(LOGGEDINUSERNAME_STRING, username);
+            return REDIRECT_SICHTUNG_MEINE_STRING;
+        } else {
+            // m.addAttribute("hinweis", hinweis);
+            m.addAttribute("username", username);
+            m.addAttribute("pwMitLaenge", pwMitLaenge);
+            m.addAttribute(LOGGEDINUSERNAME_STRING, "");
+            logger.warn("Falsche Anmeldedaten für Username: {}", username);
+            return LOGING_STRING;
+        }
+    }
+
+    @GetMapping("/logoutfoto")
+    public String logoutfoto(SessionStatus sessionstatus) {
+        // Session aktiv beenden
+        sessionstatus.setComplete();
+        return LOGING_STRING;
+    }
+
+}
