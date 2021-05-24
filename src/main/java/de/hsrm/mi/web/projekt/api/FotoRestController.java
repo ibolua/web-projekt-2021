ef3e3e7e0d66071acbef479f1c0d6d6580f647d2
Ibrahim Ay <ibrahim.ay@student.hs-rm.de>
2021-05-24 23:14:00 +0200

    Create FotoRestController und ueb07.rest

---
+++ src/main/java/de/hsrm/mi/web/projekt/api/FotoRestController.java
@@ -0,0 +1,46 @@
+package de.hsrm.mi.web.projekt.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.hsrm.mi.web.projekt.foto.Foto;
+import de.hsrm.mi.web.projekt.foto.FotoService;
+import de.hsrm.mi.web.projekt.foto.Kommentar;
+
+
+@RestController
+@RequestMapping("/api/foto")
+public class FotoRestController {
+
+    @Autowired
+    FotoService fotoservice;
+
+    Foto foto;
+
+    @GetMapping("") 
+    public List<Foto> getApiFoto() {
+        return fotoservice.alleFotosNachZeitstempelSortiert();
+    } 
+
+    @DeleteMapping("/{id}")
+    public void deleteApiFoto(@PathVariable long id) {
+        fotoservice.loescheFoto(id);
+    }
+
+    @GetMapping("/{id}/kommentar")
+    public List<Kommentar> getApiFotoIdKommentar(@PathVariable long id) {
+        return fotoservice.alleKommentareFuerFoto(id);
+    }
+
+    @DeleteMapping("/{id}/kommentar/{kid}")
+    public void deleteApiFotoIdKommentarKid(@PathVariable long id, @PathVariable long kid) {
+        fotoservice.fotoKommentarLoeschen(id, kid);
+    }
+
+}
