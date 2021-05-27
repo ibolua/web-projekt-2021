8f848fc3c50f322000b9940871c17d3b075e690d
Ibrahim Ay <ibrahim.ay@student.hs-rm.de>
2021-05-28 00:35:30 +0200

    FotoController User Session Problem  behoben

--- src/main/java/de/hsrm/mi/web/projekt/foto/FotoController.java
+++ src/main/java/de/hsrm/mi/web/projekt/foto/FotoController.java
@@ -3,7 +3,6 @@ package de.hsrm.mi.web.projekt.foto;
 import java.io.IOException;
 import java.util.Optional;
 
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,17 +11,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-// @SessionAttributes(names = { "loggedinusername" })
+@SessionAttributes(names = { "loggedinusername" })
 public class FotoController {
     private static final int MINDESTDATEIGROESSE = 17;
     private static final String FOTO_LOGIN_STRING = "foto/login"; // Compliant
@@ -79,14 +76,8 @@ public class FotoController {
     }
 
     @GetMapping("foto/{id}/kommentar")
-    public String fotoKommentarGet(@PathVariable Long id, Model m,
-            // @SessionAttribute("loggedinusername") String loggedinusername)
-            @ModelAttribute(LOGGEDINUSERNAME_STRING) String loggedinusername)
-            {
+    public String fotoKommentarGet(@PathVariable Long id, Model m) {
         Optional<Foto> foto = fotoservice.fotoAbfragenNachId(id);
-              
-        // logger.warn("GET Kommentar LOGGEDINUSERNAME SessionAttribute: {}", loggedinusername);
-        logger.warn("GET Kommentar LOGGEDINUSERNAME ModelAttribute: {}", loggedinusername);
 
         if (foto.isPresent()) {
             m.addAttribute("foto", foto.get());
@@ -96,34 +87,13 @@ public class FotoController {
     }
 
     @PostMapping("foto/{id}/kommentar")
-    public String fotoKommentarPost(@PathVariable Long id,
-            // @SessionAttribute("loggedinusername") String loggedinusername,
-            @ModelAttribute(LOGGEDINUSERNAME_STRING) String loggedinusername,
-            @RequestParam String kommentar,
-            Model m,
-            HttpSession session) {
-        logger.warn("POST Kommentar ID: {}", id);
-        logger.warn("POST Kommentar LOGGEDINUSERNAME: {}", loggedinusername);
+    public String fotoKommentarPost(@PathVariable Long id, @RequestParam String kommentar, Model m) {
+        String loggedinusernameString;
+        var loggedinusernameModelObject = m.getAttribute(LOGGEDINUSERNAME_STRING);
 
-        var test = m.getAttribute(LOGGEDINUSERNAME_STRING).toString();
-        logger.warn("TEST Kommentar LOGGEDINUSERNAME: {}", test);
-
-
-        // VERSUCH MIT HttpSession
-        var loggedinusernameSessionObject = session.getAttribute(LOGGEDINUSERNAME_STRING);
-        String loggedinusernameSTRING;
-        if(loggedinusernameSessionObject != null) {
-            loggedinusernameSTRING = loggedinusernameSessionObject.toString();
-            logger.error("/FOTO LOGGEDINUSERNAME: {}", loggedinusernameSTRING);
-        }
-        logger.error("/FOTO Kommentar sessionObject: {}", loggedinusernameSessionObject);
-        // VERSUCH MIT HttpSession
-
-        // if (isNotBlankOrIsNotEmpty(kommentar) && (isNotBlankOrIsNotEmpty(loggedinusername))) {
-        if (isNotBlankOrIsNotEmpty(kommentar) && loggedinusernameSessionObject != null) {
-            loggedinusernameSTRING = loggedinusernameSessionObject.toString();
-            fotoservice.fotoKommentieren(id, loggedinusernameSTRING, kommentar);
-            // fotoservice.fotoKommentieren(id, loggedinusername, kommentar);
+        if (isNotBlankOrIsNotEmpty(kommentar) && loggedinusernameModelObject != null) {
+            loggedinusernameString = loggedinusernameModelObject.toString();
+            fotoservice.fotoKommentieren(id, loggedinusernameString, kommentar);
         }
         return "redirect:/foto/" + id + "/kommentar";
     }
