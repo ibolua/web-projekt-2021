20d5a88a9ca934a7ee96bcd6ff81b30f76a07c3d
Ibrahim Ay <ibrahim.ay@student.hs-rm.de>
2021-06-22 23:06:02 +0200

    Delete all React frontend files

--- frontend/lib/app.js
+++
@@ -1,5 +0,0 @@
-const myElement = React.createElement('p', {}, 'Das ist mein erstes React Element');
-const myJSXElement = /*#__PURE__*/React.createElement("p", {
-  id: "my-id"
-}, "Das ist meine erste JSX Komponente");
-ReactDOM.render(myJSXElement, document.getElementById("react-app"));
